79a20886d275b5626f4e8cd79c4672cd413906e5
Daniyel Tkachuk <daniyel_github-1@mail.ru>
2023-04-19 16:41:34 +0300

    refactor HW12

--- src/s2-homeworks/hw12/HW12.tsx
+++ src/s2-homeworks/hw12/HW12.tsx
@@ -14,41 +14,41 @@ import {AppStoreType} from "../hw10/bll/store";
 * */
 
 const themes = [
-    {id: 1, value: 'light'},
-    {id: 2, value: 'blue'},
-    {id: 3, value: 'dark'},
+   {id: 1, value: 'light'},
+   {id: 2, value: 'blue'},
+   {id: 3, value: 'dark'},
 ]
 
 const HW12 = () => {
-    // взять ид темы из редакса
-    const themeId = useSelector<AppStoreType, number>(state => state.theme.themeId);
+   // взять ид темы из редакса
+   const themeId = useSelector<AppStoreType, number>(state => state.theme.themeId);
 
    const dispatch = useDispatch();
 
-    const change = (id: any) => { // дописать функцию
+   const change = (id: any) => { // дописать функцию
       dispatch(changeThemeId(+id));
-    }
+   }
 
-    useEffect(() => {
-        document.documentElement.dataset.theme = themeId + ''
-    }, [themeId])
+   useEffect(() => {
+      document.documentElement.dataset.theme = themeId + ''
+   }, [themeId])
 
-    return (
-        <div id={'hw12'}>
-            <div id={'hw12-text'} className={s2.hwTitle}>
-                Homework #12
-            </div>
+   return (
+      <div id={'hw12'}>
+         <div id={'hw12-text'} className={s2.hwTitle}>
+            Homework #12
+         </div>
 
-            <div className={s2.hw}>
-                <SuperSelect
-                    id={'hw12-select-theme'}
-                    className={s.select}
-                    options={themes}
-                    onChangeOption={change}
-                />
-            </div>
-        </div>
-    )
+         <div className={s2.hw}>
+            <SuperSelect
+               id={'hw12-select-theme'}
+               className={s.select}
+               options={themes}
+               onChangeOption={change}
+            />
+         </div>
+      </div>
+   )
 }
 
 export default HW12
--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,8 +1,7 @@
-
 export type InitialStateType = typeof initState;
 
 const initState = {
-    themeId: 1,
+    themeId: 1 as number,
 }
 
 export const themeReducer = (state: InitialStateType = initState, action: ChangeThemeIdType): InitialStateType => { // fix any
@@ -20,4 +19,6 @@ type ChangeThemeIdType = {
     type: 'SET_THEME_ID',
     id: number
 }
-export const changeThemeId = (id: number): ChangeThemeIdType => ({ type: 'SET_THEME_ID', id }) // fix any
+export const changeThemeId = (id: number): ChangeThemeIdType => {
+    return { type: 'SET_THEME_ID', id } as const
+} // fix any
