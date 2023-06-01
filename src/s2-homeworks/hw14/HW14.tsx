0ad9d4482f366b8d9e239661115a6f3c76c779d4
Daniyel Tkachuk <daniyel_github-1@mail.ru>
2023-06-02 02:34:50 +0300

    HW14 added functionality for saving data from the server

--- src/s2-homeworks/hw14/HW14.tsx
+++ src/s2-homeworks/hw14/HW14.tsx
@@ -16,7 +16,7 @@ import {useSearchParams} from 'react-router-dom'
 const getTechs = (find: string) => {
     return axios
         .get<{ techs: string[] }>(
-            'https://incubator-personal-page-back.herokuapp.com/api/3.0/homework/test2',
+            'https://samurai.it-incubator.io/api/3.0/homework/test2',
             {params: {find}}
         )
         .catch((e) => {
@@ -34,11 +34,11 @@ const HW14 = () => {
         setLoading(true)
         getTechs(value)
             .then((res) => {
-                // делает студент
+                if (res?.data?.techs) {
+                    setTechs(res.data.techs)
 
-                // сохранить пришедшие данные
-
-                //
+                }
+                setLoading(false);
             })
     }
 
@@ -54,6 +54,7 @@ const HW14 = () => {
 
     useEffect(() => {
         const params = Object.fromEntries(searchParams)
+        console.log(params.find)
         sendQuery(params.find || '')
         setFind(params.find || '')
     }, [])
